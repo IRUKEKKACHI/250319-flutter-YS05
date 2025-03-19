b2e741c2205fa0a509fd95fa17039ab654f7fb15
ydmins <dr.ydmins@gmail.com>
2025-03-19 12:29:04 +0900

    init the project

---
+++ android/app/src/main/kotlin/com/ydmins/flutter_ys05_20250319/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ydmins.flutter_ys05_20250319
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlutterYS05());
+}
+
+class FlutterYS05 extends StatelessWidget {
+  const FlutterYS05({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
