5c3337cfcd74e9656146a3f293baaf5d9113e92c
ydmins <dr.ydmins@gmail.com>
2025-03-20 13:25:34 +0900

    feat: implement Firebase anonymous login and set up essential routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_ys05_20250319/firebase_options.dart';
+import 'package:flutter_ys05_20250319/screen/chat_room_list_screen.dart';
+import 'package:flutter_ys05_20250319/screen/chat_room_screen.dart';
+import 'package:flutter_ys05_20250319/screen/create_chat_room_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  if (FirebaseAuth.instance.currentUser == null) {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } catch (e) {
+      print('Failed Anonymous Login : $e');
+    }
+  }
+
   runApp(FlutterYS05());
 }
 
 class FlutterYS05 extends StatelessWidget {
-  const FlutterYS05({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => ChatRoomListScreen(),
+        '/create': (context) => CreateChatRoomScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/detail') {
+          final args = settings.arguments as Map<String, dynamic>;
+          return null;
+        }
+        return null;
+      },
+    );
   }
 }
